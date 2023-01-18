f95db79d4ca854c51b09a8a84e7a68727fc245db
kowoonho <heesuk9898@gmail.com>
2023-01-18 01:49:30 +0000

    kwh 5th commit

--- road_following/color_filter/check_img.py
+++ road_following/color_filter/check_img.py
@@ -4,17 +4,12 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 import glob
-<<<<<<< HEAD
 #from custom_code import *
 from Algorithm.img_preprocess import *
-=======
->>>>>>> 203fa728735147d9ae4482e949a2414a1a553afd
 import sys
-
 path_rf = os.path.dirname(os.path.dirname(__file__))
 print(path_rf)
 sys.path.append(path_rf)
-from Dataset.preprocess_pdh import *
 from utility import dominant_gradient
 
 #imgs = glob.glob("./img/*.png")
