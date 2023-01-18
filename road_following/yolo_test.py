9110fc00781bb39c901836b30ee0ab98012d970b
DongHeokPark <leao8869@g.skku.edu>
2023-01-18 21:23:53 +0900

    pdh 15th commit

--- road_following/yolo_test.py
+++ road_following/yolo_test.py
@@ -4,7 +4,8 @@ import sys
 import torch
 import numpy as np
 import torchvision.transforms as transform
-sys.path.append("/home/woonho/python/1st-AD-SW-Competition/road_following/yolov5")
+path_cur = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(path_cur + "/yolov5")
 from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.general import non_max_suppression
 import matplotlib.pyplot as plt
@@ -19,6 +20,7 @@ img = cv2.imread(img_path)
 draw_img = img.copy()
 
 image = transform.functional.to_tensor(img)
+print("shape:", image.shape)
 image = image[None, ...]
 pred = model(image)
 
