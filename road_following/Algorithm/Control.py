630375e03b8a1c86169656bf08980c369227c186
kowoonho <heesuk9898@gmail.com>
2023-01-24 04:32:58 +0000

    kwh 9th commit

--- road_following/Algorithm/Control.py
+++ road_following/Algorithm/Control.py
@@ -15,63 +15,38 @@ def control_correction(road_direction, model_direction): # 예측 값과 이미�
     return direction
 
 def strengthen_control(road_direction, bottom_value): # 차선에 너무 근접한 경우 방향 수정값 증가
-    right_threshold = (370, 450, 530) ## threshold 값을 4등분해서 각 구간에 들어가면 weight값에 따라 방향 보정
-    left_threshold = (100, 180, 250)
+    # right_threshold = (370, 450, 530) ## threshold 값을 4등분해서 각 구간에 들어가면 weight값에 따라 방향 보정
+    # left_threshold = (100, 180, 250)
     
-    middle_threshold = (260, 280, 300, 340, 360, 380)
+    middle_threshold = (250, 280, 300, 310, 330, 340, 360, 390)
     left_idx, right_idx = find_nearest(bottom_value)
-    if road_direction < 0:
-        if left_idx == None or right_idx == None:
+    
+    if left_idx == None or right_idx == None:
+        if road_direction < 0:
             direction = -7
         else:
-            if right_idx > right_threshold[2]:
-                direction = int(road_direction * 0.5)
-            elif right_idx > right_threshold[1] and right_idx <= right_threshold[2]:
-                direction = int(road_direction * 1.0)
-            elif right_idx > right_threshold[0] and right_idx <= right_threshold[1]:
-                direction = int(road_direction * 1.5)
-            else:
-                direction = -7
-
-            """차선 근접도를 이용해서 direction 값 조절"""
-            
-    elif road_direction > 0:
-        if left_idx == None or right_idx == None:
             direction = 7
-        else:
-            if left_idx < left_threshold[0]:
-                direction = int(road_direction * 0.5)
-            elif left_idx >= left_threshold[0] and left_idx < left_threshold[1]:
-                direction = int(road_direction * 1.2)
-            elif left_idx >= left_threshold[1] and left_idx < left_threshold[2]:
-                direction = int(road_direction * 1.6)    
-            else:
-                direction = 7
-            """차선 근접도를 이용해서 direction 값 조절"""
     else:
-        if left_idx == None or right_idx == None:
-            if left_idx != None:
-                direction = 7
-            else:
-                direction = -7
-        else:
-            middle_lane = (left_idx + right_idx)/2
-            
-            if middle_threshold[0] > middle_lane:
-                direction = road_direction - 3
-            elif middle_threshold[0] <= middle_lane and middle_lane < middle_threshold[1]:
-                direction = road_direction - 2
-            elif middle_threshold[1] <= middle_lane and middle_lane < middle_threshold[2]:
-                direction = road_direction - 1
-            elif middle_threshold[2] <= middle_lane and middle_lane < middle_threshold[3]:
-                direction = road_direction + 0
-            elif middle_threshold[3] <= middle_lane and middle_lane < middle_threshold[4]:
-                direction = road_direction + 1
-            elif middle_threshold[4] <= middle_lane and middle_lane < middle_threshold[5]:
-                direction = road_direction + 2
-            elif middle_threshold[5] <= middle_lane:
-                direction = road_direction + 3
-            
+        middle_lane = (left_idx + right_idx)/2
+        
+        if middle_threshold[0] > middle_lane:
+            direction = road_direction - 5
+        elif middle_threshold[0] <= middle_lane and middle_lane < middle_threshold[1]:
+            direction = road_direction - 3
+        elif middle_threshold[1] <= middle_lane and middle_lane < middle_threshold[2]:
+            direction = road_direction - 2
+        elif middle_threshold[2] <= middle_lane and middle_lane < middle_threshold[3]:
+            direction = road_direction + 1
+        elif middle_threshold[3] <= middle_lane and middle_lane < middle_threshold[4]:
+            direction = road_direction + 0
+        elif middle_threshold[4] <= middle_lane and middle_lane < middle_threshold[5]:
+            direction = road_direction + 1
+        elif middle_threshold[5] <= middle_lane and middle_lane < middle_threshold[6]:
+            direction = road_direction + 2
+        elif middle_threshold[6] <= middle_lane and middle_lane < middle_threshold[7]:
+            direction = road_direction + 3
+        elif middle_threshold[7] <= middle_lane:
+            direction = road_direction + 5
             
         
     direction = 7 if direction >= 7 else direction
