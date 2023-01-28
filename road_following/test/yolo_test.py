003bcebad43dcc3d70761f4e007835ff63cfddf9
kowoonho <heesuk9898@gmail.com>
2023-01-28 13:24:54 +0900

    kwh 13th commit

--- road_following/test/yolo_test.py
+++ road_following/test/yolo_test.py
@@ -3,48 +3,36 @@ import os
 import sys
 import torch
 import numpy as np
-import torchvision.transforms as transform
-<<<<<<< HEAD:road_following/yolo_test.py
-path_cur = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(path_cur + "/yolov5")
-=======
+from glob import glob
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-
-path_rf = os.path.dirname(__file__)
-print(path_rf)
-sys.path.append(path_rf + "/yolov5")
->>>>>>> 98bd515cde1da8e7bccd4a10c4c2cd44a2580c26:road_following/test/yolo_test.py
-=======
 from pathlib import Path
-rf_path = str(Path(os.getcwd()).parent)
-sys.path.append(rf_path)
->>>>>>> d558718f354929b33466f91784e7854e42cf28f7
+PATH = str(Path(os.path.dirname(__file__)).parent)
+sys.path.append(os.path.join(PATH, 'yolov5'))
 from yolov5.models.common import DetectMultiBackend
 from yolov5.utils.general import non_max_suppression
 from utility import preprocess, show_bounding_box
-labels_to_names = {0 : "Crosswalk", 1 : "Green", 2 : "Red"}
+labels_to_names = {0 : "Crosswalk", 1 : "Green", 2 : "Red", 3 : "Car"}
 
-weight_file_path = os.path.join(rf_path, 'model_weight_file', 'yolo_weight.pt')
+weight_file_path = os.path.join(PATH, 'model_weight_file', 'yolo_final_weight.pt')
 model = DetectMultiBackend(weights = weight_file_path)
 
-img_path = os.path.join(rf_path, 'test_image', 'trafficlight.png')
+img_path = os.path.join(PATH, 'test_image')
+img_list = glob(os.path.join(img_path, "car*"))
+
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
-img = cv2.imread(img_path)
-draw_img = img.copy()
 
-<<<<<<< HEAD
-image = transform.functional.to_tensor(img)
-print("shape:", image.shape)
-image = image[None, ...]
-=======
-image = preprocess(img, "test")
-image = image.cpu()
->>>>>>> e8e61532ae354662fa24e87293a5fdbeeaddf0f2
-pred = model(image)
-pred = non_max_suppression(pred)[0]
+for i in range(5):
+    img = cv2.imread(img_list[i])
+    draw_img = img.copy()
+    image = preprocess(img, 'test').cpu()
+    pred = model(image)
+    pred = non_max_suppression(pred)[0]
+    
+    draw_img, detect_list = show_bounding_box(draw_img, pred)
+    
+    plt.subplot(1, 5, i+1)
+    plt.imshow(draw_img)
 
-draw_img, detect_list= show_bounding_box(draw_img, pred)
-
-print(detect_list)
+plt.show()
